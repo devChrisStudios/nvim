ee1277fc808b2e14d8add3f91d1de7b749c3f157
Christian <devchrisstudios@gmail.com>
2026-01-30 03:38:13 -0600

    nvim config 3:37AM

--- lua/remap.lua
+++ lua/remap.lua
@@ -34,9 +34,7 @@ vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true }) -- M
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > " ) })
-end)
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 
 -- Harpoon
 local mark = require("harpoon.mark")
