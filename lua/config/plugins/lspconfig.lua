e9fea672f68ec8c4a7d72ffb4032c7d03fab3fc1
Christian <devchrisstudios@gmail.com>
2026-01-31 20:35:31 -0600

    setup lsp for js and stuff

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -3,7 +3,23 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
   config = function()
-	  vim.lsp.enable('pyright', 'ruff', 'copilot', 'lua-ls', 'tsserver', 'html', 'cssls', 'rust_analyzer')
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true 
+
+      vim.lsp.config('html', {
+          capabilities = capabilities,
+      }, 'cssls', {
+          capabilities = capabilities,
+      }, 'rust_analyzer', {
+          settings = {
+                ["rust-analyzer"] = {
+                    diagnostics = {
+                        enable = true,
+                    },
+                },
+            },
+        })
+        vim.lsp.enable('pyright', 'ruff', 'copilot', 'lua-ls', 'ts_ls', 'html', 'cssls', 'rust_analyzer')
   end
 }
 
