3ca4025e2535059e3c57ae726380793fbfeeb7db
Christian <devchrisstudios@gmail.com>
2026-01-31 20:10:51 -0600

    nvim commit

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
   config = function()
-	  vim.lsp.enable('pyright', 'ruff', 'copilot', 'lua-ls')
+	  vim.lsp.enable('pyright', 'ruff', 'copilot', 'lua-ls', 'tsserver', 'html', 'cssls', 'rust_analyzer')
   end
 }
 
