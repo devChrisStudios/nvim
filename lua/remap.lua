0d8343f9b8a34460d18891ab8420723c0188dfae
Christian <devchrisstudios@gmail.com>
2026-02-18 15:14:13 -0600

    fucking C-navkeys are so good and now i have to use them for fucking tmux doodj

--- lua/remap.lua
+++ lua/remap.lua
@@ -56,10 +56,10 @@ vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 
 -- UndoTree
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+--vim.keymap.set('n', '<C-h>', '<C-w>h')
+--vim.keymap.set('n', '<C-j>', '<C-w>j')
+--vim.keymap.set('n', '<C-k>', '<C-w>k')
+--vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 -- Fugitive
 vim.keymap.set("n", "<leader>gs", ":Git<CR>") -- Git Status 
