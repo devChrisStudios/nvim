52a4aabf8c491dadb5dad596530157c71360b07a
Christian <devchrisstudios@gmail.com>
2026-02-24 22:15:36 -0600

    remap revamp

--- lua/remap.lua
+++ lua/remap.lua
@@ -37,11 +37,11 @@ vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = true }) -- M
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set("n", "<C-f>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pc", function() builtin.find_files({ cwd = vim.fn.stdpath("config")}) end, {})
 vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 
 -- Harpoon
 local mark = require("harpoon.mark")
