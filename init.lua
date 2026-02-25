9c6bef89c6a6e9f3e947abe036e62dc6af08fe14
Christian <devchrisstudios@gmail.com>
2026-02-25 00:43:39 -0600

    idfk at this point

--- init.lua
+++ init.lua
@@ -3,7 +3,3 @@ require("config.lazy")
 require("remap")
 require("set")
 
---require('mini.snippets').setup({})
---require('mini.completion').setup({})
-
---vim.lsp.enable('pyright', 'ruff', 'copilot', 'lua-ls')
