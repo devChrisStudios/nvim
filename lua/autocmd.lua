4558e3404629b258979d80c7d9363298c5019a92
Christian <devchrisstudios@gmail.com>
2026-03-09 09:35:16 -0500

    flash and telescope tweaks, still cant get that fucking preview working tho goddamn

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -27,3 +27,4 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,10 +1,18 @@
 
 return {
 	{
-		'nvim-telescope/telescope.nvim', version = '*',
+		'nvim-telescope/telescope.nvim',
 		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{ 'nvim-telescope/telescope-fzf-native.nvim' },
-		}
+			{ 'nvim-lua/plenary.nvim' },
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		},
+		config = function()
+			require('telescope').setup({
+				defaults = {
+					layout_strategy = 'flex',
+					preview_cutoff = 0,
+				},
+			})
+		end,
 	}
 }
